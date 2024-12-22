556a1dfd2aed72a3448ac179fd54dec85f7e5f63
Ijndi <ijndidi1229@gmail.com>
2024-12-22 20:03:08 +0000

    28th commit

---
+++ week 4/objects.js
@@ -0,0 +1,13 @@
+// below is an object literal form
+const bike = {
+    type:"mountain bike",
+    model:"carrera",
+    color:"black",
+    wheel_size:"26 inches"
+}
+
+const car={}
+car.type = "Porsche";
+car.model = "992 GT3 RS 4l V6";
+car.color = "black";
+car.price = 200000;
